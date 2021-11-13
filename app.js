17f883cc6c83803e17b8f6f977b84d40a27d1df0
Rostyslav7 <rostyk178@gmail.com>
2021-11-13 15:38:43 +0200

    add conditions and

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
+'use strict'
+
 const numberOfFilms = +prompt('Скільки фільмів ви подивилися', '10');
+  
 
 const personalMovieDB = {
   count : numberOfFilms,
-  movies : {}, // movies: {harry: '4', volk: '7'}
+  movies : {}, 
   actors : {},
   genres : [],
   privat : false
 };
 
-let a = prompt('Який останній фільм ви подивилися?',''),
-    b = prompt('Наскільки оцінити його?',''),
-    c = prompt('Який останній фільм ви подивилися?',''),
-    d = prompt('Наскільки оцінити його?','');
 
-personalMovieDB.movies[a] = b; 
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++ ) {
+  let a = prompt('Який останній фільм ви подивилися?',''),
+      b = prompt('Наскільки оцінити його?','');
 
-console.log(personalMovieDB);
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b; 
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+  
+}
+if(personalMovieDB.count < 10) {
+  console.log('Мало фільмів переглянуто'); 
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log('Ви класичний глядач');
+} else if (personalMovieDB.count >= 30 ) {
+  console.log('Ви кіноман');
+} else {
+  console.log('Виникла помилка');
+}
+console.log(personalMovieDB)
+
+
